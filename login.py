37278141dcb7237077847059315a7928c2fbad28
Bones <bone_ace@163.com>
2017-05-09 09:43:21 +0800

    WeiboSliderCode_v2.0版本

--- login.py
+++ login.py
@@ -8,6 +8,7 @@
 
 import time
 import random
+import StringIO
 from PIL import Image
 from math import sqrt
 from ims import ims
@@ -15,22 +16,51 @@ from selenium import webdriver
 from selenium.webdriver.remote.command import Command
 from selenium.webdriver.common.action_chains import ActionChains
 
-PIXELS = [(465, 371), (566, 371), (465, 472), (566, 472)]
+PIXELS = []
+
+
+def getExactly(im):
+    """ 精确剪切"""
+    imin = -1
+    imax = -1
+    jmin = -1
+    jmax = -1
+    row = im.size[0]
+    col = im.size[1]
+    for i in range(row):
+        for j in range(col):
+            if im.load()[i, j] != 255:
+                imax = i
+                break
+        if imax == -1:
+            imin = i
+
+    for j in range(col):
+        for i in range(row):
+            if im.load()[i, j] != 255:
+                jmax = j
+                break
+        if jmax == -1:
+            jmin = j
+    return (imin + 1, jmin + 1, imax + 1, jmax + 1)
 
 
 def getType(browser):
     """ 识别图形路径 """
     ttype = ''
     time.sleep(3.5)
-    browser.save_screenshot('aa.png')
-    im = Image.open('aa.png').crop((400, 310, 630, 530)).convert('L')
+    im0 = Image.open(StringIO.StringIO(browser.get_screenshot_as_png()))
+    box = browser.find_element_by_id('patternCaptchaHolder')
+    im = im0.crop((int(box.location['x']) + 10, int(box.location['y']) + 100, int(box.location['x']) + box.size['width'] - 10, int(box.location['y']) + box.size['height'] - 10)).convert('L')
+    newBox = getExactly(im)
+    im = im.crop(newBox)
     width = im.size[0]
     height = im.size[1]
     for png in ims.keys():
         isGoingOn = True
         for i in range(width):
             for j in range(height):
-                if abs(im.load()[i, j] - ims[png][i][j]) > 10:
+                if ((im.load()[i, j] >= 245 and ims[png][i][j] < 245) or (im.load()[i, j] < 245 and ims[png][i][j] >= 245)) and abs(ims[png][i][j] - im.load()[i, j]) > 10: # 以245为临界值，大约245为空白，小于245为线条；两个像素之间的差大约10，是为了去除245边界上的误差
                     isGoingOn = False
                     break
             if isGoingOn is False:
@@ -40,29 +70,36 @@ def getType(browser):
                 ttype = png
         else:
             break
+    px0_x = box.location['x'] + 40 + newBox[0]
+    px1_y = box.location['y'] + 130 + newBox[1]
+    PIXELS.append((px0_x, px1_y))
+    PIXELS.append((px0_x + 100, px1_y))
+    PIXELS.append((px0_x, px1_y + 100))
+    PIXELS.append((px0_x + 100, px1_y + 100))
     return ttype
 
 
 def move(browser, coordinate, coordinate0):
     """ 从坐标coordinate0，移动到坐标coordinate """
     time.sleep(0.05)
-    length = sqrt((coordinate[0] - coordinate0[0]) ** 2 + (coordinate[1] - coordinate0[1]) ** 2)    # 两点直线距离
+    length = sqrt((coordinate[0] - coordinate0[0]) ** 2 + (coordinate[1] - coordinate0[1]) ** 2)  # 两点直线距离
     if length < 4:  # 如果两点之间距离小于4px，直接划过去
-        ActionChains(browser).move_by_offset(coordinate[0]-coordinate0[0], coordinate[1]-coordinate0[1]).perform()
+        ActionChains(browser).move_by_offset(coordinate[0] - coordinate0[0], coordinate[1] - coordinate0[1]).perform()
         return
-    else:   # 递归，不断向着终点滑动
+    else:  # 递归，不断向着终点滑动
         step = random.randint(3, 5)
-        x = int(step * (coordinate[0] - coordinate0[0]) / length)   # 按比例
+        x = int(step * (coordinate[0] - coordinate0[0]) / length)  # 按比例
         y = int(step * (coordinate[1] - coordinate0[1]) / length)
         ActionChains(browser).move_by_offset(x, y).perform()
-        move(browser, coordinate, (coordinate0[0]+x, coordinate0[1]+y))
+        move(browser, coordinate, (coordinate0[0] + x, coordinate0[1] + y))
 
 
 def draw(browser, ttype):
     """ 滑动 """
     if len(ttype) == 4:
         px0 = PIXELS[int(ttype[0]) - 1]
-        ActionChains(browser).move_by_offset(px0[0] - 516, px0[1] - 304).perform()
+        login = browser.find_element_by_id('loginAction')
+        ActionChains(browser).move_to_element(login).move_by_offset(px0[0] - login.location['x'] - int(login.size['width'] / 2), px0[1] - login.location['y'] - int(login.size['height'] / 2)).perform()
         browser.execute(Command.MOUSE_DOWN, {})
 
         px1 = PIXELS[int(ttype[1]) - 1]
@@ -87,13 +124,12 @@ if __name__ == '__main__':
     name = browser.find_element_by_id('loginName')
     psw = browser.find_element_by_id('loginPassword')
     login = browser.find_element_by_id('loginAction')
-    name.send_keys('15200692422')   # 测试账号
+    name.send_keys('15200692422')  # 测试账号
     psw.send_keys('zckhm7071')
     login.click()
 
-    ttpye = getType(browser)    # 识别图形路径
-    print 'Result: %s!' % ttpye
-    draw(browser, ttpye)    # 滑动破解
+    ttype = getType(browser)  # 识别图形路径
+    print 'Result: %s!' % ttype
+    draw(browser, ttype)  # 滑动破解
     time.sleep(20)
     browser.close()
-
